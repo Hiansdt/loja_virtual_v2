6bdb26bf0ee109ba7da025b6a330edafcc18ad1d
Hiansdt <vinijao123690@gmail.com>
2024-07-10 15:12:46 -0300

    first commit

---
+++ src/components/templates/small/SmallAside.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Small Aside</h2>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
+<template>
+  <div class="authContainer">
+    <passage-auth
+      app-id="jwi7hT7o7cfHOjvhGVa622sN"
+    ></passage-auth>
+  </div>
+</template>
+
+<style></style>
